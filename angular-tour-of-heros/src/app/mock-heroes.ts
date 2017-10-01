7103d3dd3a3fd9fd54d43f5441481a6a134c3d12
Vishal Nananiya <vishal.nanania@gmail.com>
2017-09-30 23:57:06 -0400

    added new heroes..

--- angular-tour-of-heros/src/app/mock-heroes.ts
+++ angular-tour-of-heros/src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Batman' },
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Iron Man' },
+  { id: 14, name: 'Captain America' },
+  { id: 15, name: 'Spiderman' },
+  { id: 16, name: 'The Flash' },
+  { id: 17, name: 'Wolverine' },
+  { id: 18, name: 'The Hulk' },
+  { id: 19, name: 'Hellboy' },
+  { id: 20, name: 'Deadpool' }
 ];
