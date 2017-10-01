6b5a9202ee4ad6c507a01f288ff67344aa92fa15
Vishal Nananiya <vishal.nanania@gmail.com>
2017-09-30 23:48:40 -0400

    added routing..

--- angular-tour-of-heros/src/app/app.component.ts
+++ angular-tour-of-heros/src/app/app.component.ts
@@ -1,39 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-  <div class="container">
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-      <div class="list-group">
-        <a href="#" class="list-group-item"
-           *ngFor="let hero of heroes"
-          [class.active]="hero === selectedHero"
-          (click)="onSelect(hero)">
-          {{hero.id}} : {{hero.name}}</a>
-      </div>
-    <hero-detail [hero]="selectedHero"></hero-detail>
-  </div>
-  `,
-  providers: [HeroService]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) { }
-   getHeroes(): void {
-      this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
-    }
-    ngOnInit(): void {
-      this.getHeroes();
-    }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
