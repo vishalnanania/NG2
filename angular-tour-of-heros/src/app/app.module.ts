1969f27ff0e0edab8f9062a2177bcd13aa0b0883
Vishal Nananiya <vishal.nanania@gmail.com>
2017-10-05 15:36:06 -0400

    added search functionality.

--- angular-tour-of-heros/src/app/app.module.ts
+++ angular-tour-of-heros/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroSearchComponent }  from './hero-search.component';
 import { HeroService }          from './hero.service';
 
 @NgModule({
@@ -28,6 +29,7 @@ import { HeroService }          from './hero.service';
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
