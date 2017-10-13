10193d5dbcab0ca20fe31d7d17134f2ba91b424c
Vishal Nananiya <vishal.nanania@gmail.com>
2017-10-13 14:35:08 -0400

    internal html and style. template and styles properties.

--- angular-food-app/src/app/app.component.ts
+++ angular-food-app/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //template: ``, html template here.
+  //styles: `[{}]` some style here
 })
 export class AppComponent {
   title = 'app';
