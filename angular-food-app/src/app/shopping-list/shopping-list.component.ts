3345fe0f622cbf5788ef5cb36acabb2f8d37f178
Vishal Nananiya <vishal.nanania@gmail.com>
2017-10-24 13:18:16 -0400

    added ingredient model.

--- angular-food-app/src/app/shopping-list/shopping-list.component.ts
+++ angular-food-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomato',5),
+    new Ingredient('Cucumber',5),
+  ];
 
   constructor() { }
 
