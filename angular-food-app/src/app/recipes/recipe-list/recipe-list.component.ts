74f3348137b94273bd9df4c9c96320ba263d8c5a
Vishal Nananiya <vishal.nanania@gmail.com>
2017-10-24 12:07:49 -0400

    dynamic recipe list data.

--- angular-food-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-food-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+      new Recipe('Subway', 'vegetable footlong', 'https://i.pinimg.com/originals/d0/c9/53/d0c95321eb3c28ffdd880593ef1305d3.jpg')
+  ];
 
   constructor() { }
 
