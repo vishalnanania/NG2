9fec45d38ea04697576353154a70fb61a200eefc
Vishal Nananiya <vishal.nanania@gmail.com>
2017-11-02 23:33:39 -0400

    local reference accessing in typescript by using @ViewChild decorator.

--- angular-food-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angular-food-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput') nameInput;
   constructor() { }
 
+  onAdd(){
+    console.log(this.nameInput.nativeElement.value);
+  }
   ngOnInit() {
+
   }
 
 }
