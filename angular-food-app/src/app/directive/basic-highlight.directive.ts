2bc38469cbd2215c777c364d5c2cfc85c37f63b4
Vishal Nananiya <vishal.nanania@gmail.com>
2017-11-07 10:45:45 -0500

    custom directive with use of @HostListener and @HostBinding decorators.

--- angular-food-app/src/app/directive/basic-highlight.directive.ts
+++ angular-food-app/src/app/directive/basic-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
@@ -11,4 +11,24 @@ export class BasicHighlightDirective {
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'red');
     //this.elementRef.nativeElement.style.color = "red";
   }
+
+  @HostBinding('style.color') color = 'red';
+
+  @HostListener('mouseenter') mouseenter(eventData){
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'green');
+    this.color = 'green';
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData){
+    this.color = 'red';
+  }
+
+  @HostListener('mousedown') mousedown(eventData){
+      this.color = 'yellow';
+  }
+
+  @HostListener('mouseup') mouseup(eventData){
+      this.color = 'green';
+  }
+
 }
