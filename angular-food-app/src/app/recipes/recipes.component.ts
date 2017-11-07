9a9ce35ff8e8a8afc068bc9cff63e53b94cddcf8
Vishal Nananiya <vishal.nanania@gmail.com>
2017-11-07 13:31:16 -0500

    [ngSwitch], *ngSwitchCase, *ngSwitchDefault

--- angular-food-app/src/app/recipes/recipes.component.ts
+++ angular-food-app/src/app/recipes/recipes.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RecipesComponent implements OnInit {
 
   selectedRecipe;
+  switchTest="";
   onSelectedRecipe3(selectedRecipe){
     console.log(selectedRecipe);
     this.selectedRecipe = selectedRecipe;
