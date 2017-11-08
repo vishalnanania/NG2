87b7d0bc184efff0eb4a7df8062acaa9b0a08f27
Vishal Nananiya <vishal.nanania@gmail.com>
2017-11-08 11:22:44 -0500

    Adding Ingredients to recipe by using service and model.

--- angular-food-app/src/app/recipes/recipe.service.ts
+++ angular-food-app/src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
@@ -9,7 +10,33 @@ export class RecipeService {
     constructor() { }
 
     private recipes: Recipe[] = [
-          new Recipe('Subway', 'vegetable footlong', 'https://i.pinimg.com/originals/d0/c9/53/d0c95321eb3c28ffdd880593ef1305d3.jpg')
+          new Recipe('Subway',
+                  'vegetable footlong',
+                  'https://i.pinimg.com/originals/d0/c9/53/d0c95321eb3c28ffdd880593ef1305d3.jpg',
+                  [ new Ingredient('Bread',1),
+                    new Ingredient('Tomato',8),
+                    new Ingredient('Banana Papers',4),
+                    new Ingredient('Cucumber',8),
+                    new Ingredient('Lettuce',10),
+                    new Ingredient('Onion',4),
+                    new Ingredient('Pickle',4),
+                    new Ingredient('American Cheese',4),
+                    new Ingredient('Chipotle Southwest',2),
+                  ]
+                  ),
+          new Recipe('Burger',
+                    'vegetable Burger',
+                    'http://bk-apac-prd.s3.amazonaws.com/sites/burgerking.co.nz/files/BK_Salad-Burger-Detail.png',
+                    [ new Ingredient('Buns',1),
+                      new Ingredient('Tomato',3),
+                      new Ingredient('Patty',1),
+                      new Ingredient('Cucumber',8),
+                      new Ingredient('Lettuce',5),
+                      new Ingredient('Onion',3),
+                      new Ingredient('Pickle',3),
+                      new Ingredient('American Cheese',2)
+                    ]
+                    )
     ];
 
     getRecipes(){
