fdfe7e9d53df52f103551a452773c19e964dc870
Vishal Nananiya <vishal.nanania@gmail.com>
2017-11-13 11:06:17 -0500

    Observable and subscribe methods.

--- angular-food-app/src/app/app.component.ts
+++ angular-food-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +9,12 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
 export class AppComponent {
-    recipeComponent = true;
-    shoppingListComponent = false;
-
-    changeView(viewData){
-        this.recipeComponent = viewData.recipeComponent;
-        this.shoppingListComponent = viewData.shoppingListComponent;
-    }
+  ngOnInit() {
+    const myNumber = Observable.interval(1000);
+      myNumber.subscribe(
+        (number: Number) => {
+          console.log(number);
+        }
+      )
+  }
 }
