3ca924997f3a5b2538659cf6c54c3c47f5189b30
Vishal Nananiya <vishal.nanania@gmail.com>
2017-11-13 11:42:17 -0500

    Observable, Observer, Subscribe, NgOnDestroy, unsubscribe...

--- angular-food-app/src/app/app.component.ts
+++ angular-food-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/Rx';
 
 @Component({
@@ -9,12 +11,42 @@ import 'rxjs/Rx';
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
 export class AppComponent {
+  myNumberSubscription: Subscription;
+  myCustNumberSubscription: Subscription;
+
   ngOnInit() {
     const myNumber = Observable.interval(1000);
-      myNumber.subscribe(
-        (number: Number) => {
-          console.log(number);
-        }
-      )
+    this.myNumberSubscription = myNumber.subscribe(
+      (number: Number) => {
+        console.log(number);
+      }
+    );
+
+    const myCustNumber = Observable.create((observer: Observer<string>)=>{
+      setTimeout(()=>{
+        observer.next('first');
+      },2000);
+      setTimeout(()=>{
+        observer.next('second');
+      },4000);
+      setTimeout(()=>{
+        observer.complete();
+      },6000);
+      setTimeout(()=>{
+        observer.error('error');
+      },6000);
+
+    });
+
+    this.myCustNumberSubscription = myCustNumber.subscribe(
+      (data: string)=>{ console.log(data);},
+      (error: string)=>{ console.log(error);},
+      ()=>{ console.log("completed");}
+    );
+  }
+
+  NgOnDestroy(){
+    this.myNumberSubscription.unsubscribe();
+    this.myCustNumberSubscription.unsubscribe();
   }
 }
