a9a2a951d75a0ed0f2a5926d7e466f4431819f3e
Vishal Nananiya <vishal.nanania@gmail.com>
2017-11-13 22:34:52 -0500

    angular form. (ngSubmit)="onAdd(f)" #f="ngForm" ngModel NgForm

--- angular-food-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angular-food-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -9,15 +10,14 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInput;
-  @ViewChild('amountInput') amountInput;
 
   constructor(private shoppingListService: ShoppingListService) {
 
   }
 
-  onAdd(){
-    this.shoppingListService.addIngredient(new Ingredient(this.nameInput.nativeElement.value,this.amountInput.nativeElement.value));
+  onAdd(form: NgForm){
+    const formData = form.value;
+    this.shoppingListService.addIngredient(new Ingredient(formData.name,formData.amount));
   };
 
   ngOnInit() {
