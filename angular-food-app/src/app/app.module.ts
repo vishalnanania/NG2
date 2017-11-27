1f0d4453aab26a14f5ae8ab962a7b6bd307660bc
Vishal Nananiya <vishal.nanania@gmail.com>
2017-11-27 17:50:02 -0500

    StoreRouterConnectingModule, StoreDevtoolsModule

--- angular-food-app/src/app/app.module.ts
+++ angular-food-app/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { CoreModule } from "./core/core.module";
 import { reducers } from "./store/app.reducers";
 import {EffectsModule} from "@ngrx/effects";
 import {AuthEffects} from "./auth/store/auth.effects";
+import {StoreRouterConnectingModule} from "@ngrx/router-store";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
 
 
 @NgModule({
@@ -30,7 +33,9 @@ import {AuthEffects} from "./auth/store/auth.effects";
     AuthenticateModule,
     CoreModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreRouterConnectingModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
